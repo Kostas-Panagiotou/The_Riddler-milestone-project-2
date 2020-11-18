84cda491f021ffd7206e68b16a7756a9f3e6144a
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2020-11-18 13:26:01 +0000

    modified index.html and style.css change values and trying to be more responsive also change the time of the game the most important add the script.js a function when you call it in html and react with the overlays on html to js

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,16 @@
+ // create a function to call when the html is ready with the cards and the overlays on the screen
+$(document).ready (function() {
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+    
+     //remove the visisble on html and when you click on screen the overlays goes away and ready to start//                                                                                
+    overlays.forEach(overlay  =>{
+    overlay.addEventListener('click' , () => {
+    overlay.classList.remove('visible');
+   
+ });
+    });    
+});
 
 
+    
