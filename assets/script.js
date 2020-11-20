06db036f9ede8a7f3df78ab1f1bf11b26933578c
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2020-11-20 15:33:18 +0000

    create shuffle function algorithm by fisher and yates create a start countdown function and game-over function and also a winner function in this moment seems working the clock count down function and also the win and the game-over but i have some errors in the console i try to figure out what is the cause of it

--- assets/script.js
+++ assets/script.js
@@ -14,9 +14,26 @@ class TheRiddler {
       this.timeRemaining = this.totalTime;
       this.matchedCards = [];
       this.occupy = true;
+      
+      setTimeout(() => {
+    this.shuffleCards();
+    this.countDown = this.startCountDown();
+    this.occupy = false;
+    }, 500);
+    
+    this.hideCards();
+    this.timer.innerText = this.timeRemaining;
+    this.ticker.innerText = this.totalClicks;
 }
+   hideCards() {
+        this.cardsArray.forEach(card => {
+        card.classList.remove('visible');
+            
+    });
+}   
+    
 
-  // flip card + dont flip value and if statement and add total clicks to view on the screen//
+  // flip card + dont flipcard value and if statement and add total clicks to view on the screen//
 flipCard(card) {
     if(this.dontFlipCard(card)) {
        this.totalClicks++;
@@ -24,6 +41,38 @@ flipCard(card) {
        card.classList.add('visible');  
     }
 }
+  // create a start countdown function to count the time of remaining time of the game
+  // create  a game-over function when the time pass you have a game over screen
+  // create also a winner function when you matched all the cards//
+ startCountDown() {
+        return setInterval(() => {
+            this.timeRemaining--;
+            this.timer.innerText = this.timeRemaining;
+            
+            if(this.timeRemaining === 0)
+            this.gameover();
+        },  1000);
+    }
+ gameover() {
+        clearInterval(this.countDown);
+        document.getElementById('game-over-text').classList.add('visible');
+    }
+
+  winner() {
+      clearInterval(this.countDown);
+      document.getElementById('win-text').classList.add('visible');
+  }
+
+//Fisher–Yates_shuffle Algorithm  shuffle the cards  to end and back to start shuffle the cards through the css style order//
+shuffleCards () {
+        for(let i = this.cardsArray.length  -1; i > 0; i--) {
+        let randomIndex = Math.floor(Math.random() * (i+1));
+        this.cardsArray[randomIndex].style.order = i;
+        this.cardsArray[i].style.order = randomIndex;
+        }
+    }
+
+
 dontFlipCard(card) {
         return true;
         
@@ -32,12 +81,11 @@ dontFlipCard(card) {
 }
  
 
-
  // create a function to call when the html is ready with the cards and the overlays on the screen
  $(document).ready (function() {
     let overlays =Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
-    let game = new TheRiddler(49, cards);
+    let game = new TheRiddler(39, cards);
     
     //remove the visisble on html and when you click on screen the overlays goes away and ready to start//                                                                                
     overlays.forEach(overlay  => {
